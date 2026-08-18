d2f839f9ae80f36b9e5c805efd2a266158e387f0
fahimeh_alipour <fahimeh.alipour.73@gmail.com>
2026-08-18 09:05:23 +0330

    create structure of project

---
+++ face detection transformer/data/dataLoader.py
@@ -0,0 +1,16 @@
+import os 
+import numpy as np
+import torch
+import torch.utils.data import DataLoader, Dataset, Subset
+
+
+
+class FaceDetectionDataset(Dataset):
+    def __init__(self):
+        pass
+
+
+
+
+def makeLoader(traindata, valdata,batchsize):
+    pass
